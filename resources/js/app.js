ec67a24e85260ccf5f4d5cbc3a3d912dd9b18912
Volodymyr Komarov <vovikems@gmail.com>
2019-05-14 17:42:53 +0300

    Add auth scaffold

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,10 @@ Vue.component('example-component', ExampleComponent);
 const app = new Vue({
     el: '#app',
 });
+
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
